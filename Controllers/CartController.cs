63a580a6fd3160b58a4ee6fccbf658d9cd110fc1
Malek <malekaltwoqi@gmail.com>
2024-03-17 03:15:48 +0300

    Food-3#time 4h#done

---
+++ Controllers/CartController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Contracts;
+using OrderService.Entities.Model;
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   // [ApiConventionType(typeof(DefaultApiConventions))]
+
+
+    public class CartController : ControllerBase
+
+    {
+
+        private readonly IUnitOfWork _uniftOfWork;
+        
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _uniftOfWork = unitOfWork;
+        }
+     /*   public void EditItem(int id)
+
+        {
+            var item = _uniftOfWork.Carts.GetById(id);
+            if (item != null)
+            {
+                _uniftOfWork.Carts.Update(item);
+                _uniftOfWork.Complete();
+            }
+        }*/
+
+        
+       
+
+       
+        [HttpPut ]
+        public async Task<IActionResult> EditItem(int id, Orders order)
+        {
+            if (id != order.OrderID)
+            {
+                return BadRequest();
+            }
+
+            _uniftOfWork.Order.Update(order);
+
+            try
+            {
+                _uniftOfWork.Complete();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    EditItem(id,order);
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool OrderExists(int id)
+        {
+            return (_uniftOfWork.Order.GetAll()?.Any(e =>e.OrderID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Migrations/20240316231319_OrderServiceCon.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderServiceCon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "OrderID",
+                table: "orders",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "OrderID",
+                table: "orders",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
