7ca7246567b5c773116c0edca31d781794db9148
iCtz <ahmed.t.alfusail@gmail.com>
2024-03-19 05:12:19 +0300

    FOOD-28 FOOD-5 #time 5m #in-progress FOOD-28 FOOD-5 #comment add CartConfig entity based on anwar code

---
+++ Entities/Configs/CartsConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Entities.Model;
+
+namespace OrderService.Entities.Configs
+{
+    public class CartsConfiguration : IEntityTypeConfiguration<Cart>
+    {
+        /// <summary>
+        /// Create a composite key for [CustomerID, ProductID]
+        /// </summary>
+        /// <param name="builder"></param>
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.HasKey(c => new { c.CustomerID, c.ProductID });
+        }
+    }
+
+}
