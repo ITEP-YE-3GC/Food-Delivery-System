4f138fd6ea77d14f90f6de09e0497a69f55a01e6
iCtz <ahmed.t.alfusail@gmail.com>
2024-03-23 22:28:23 +0300

    FOOD-28 FOOD-5 #time 5m #in-progress FOOD-28 FOOD-5 #comment add order auto mapper class

---
+++ Entities/Model/DTOs/OrderAutoMapper.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System.Net.Sockets;
+
+namespace OrderService.Entities.Model.DTOs
+{
+    public class OrderAutoMapper : Profile
+    {
+        public OrderAutoMapper()
+        {
+            CreateMap<OrderAddDto, Order>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreateDate, opt => opt.Ignore())
+                .ForMember(dest => dest.OrderDetails, opt => opt.MapFrom(src => src.OrderDetails))
+                .ForMember(dest => dest.OrderCustomization, opt => opt.MapFrom(src => src.OrderCustomization));
+
+            CreateMap<OrderDetailsAddDto, OrderDetails>()
+                .ForMember(dest => dest.OrderID, opt => opt.Ignore());
+
+            CreateMap<OrderCustomizationAddDto, OrderCustomization>()
+                .ForMember(dest => dest.OrderID, opt => opt.Ignore());
+
+            CreateMap<OrderUpdateDto, Order>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreateDate, opt => opt.Ignore())
+                .ForMember(dest => dest.OrderDetails, opt => opt.MapFrom(src => src.OrderDetails))
+                .ForMember(dest => dest.OrderCustomization, opt => opt.MapFrom(src => src.OrderCustomization));
+        }
+    }
+}
