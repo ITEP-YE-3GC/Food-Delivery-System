7221647a92da320d569ebc2e06ed891d1e7e7f8a
iCtz <ahmed.t.alfusail@gmail.com>
2024-03-23 21:36:21 +0300

    FOOD-28 FOOD-5 #time 5m #in-progress FOOD-28 FOOD-5 #comment add order customization class

---
+++ Entities/Model/OrderCustomization.cs
@@ -0,0 +1,17 @@
+﻿namespace OrderService.Entities.Model
+{
+    public class OrderCustomization
+    {
+
+        public Guid OrderID { get; set; }
+        public long Seq { get; set; }
+
+        public int CustomizationID { get; set; }
+
+        public double Price { get; set; } = 0;
+
+        public int Quantity { get; set; }
+
+        public bool ValidQuantity => Quantity >= Constants.ValidQuantity;
+    }
+}
