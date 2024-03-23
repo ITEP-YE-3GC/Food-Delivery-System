be7f809bff16c524ba08943003931e48908fede6
iCtz <ahmed.t.alfusail@gmail.com>
2024-03-23 22:26:31 +0300

    FOOD-28 FOOD-5 #time 5m #in-progress FOOD-28 FOOD-5 #comment add order UpdateDto class

---
+++ Entities/Model/DTOs/OrderUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using OrderService.Utilities;
+
+namespace OrderService.Entities.Model.DTOs
+{
+    public class OrderUpdateDto
+    {
+        public int CustomerID { get; set; }
+        public int DriverID { get; set; }
+        public int AddressID { get; set; }
+        public int PaymentID { get; set; }
+        public int StatusID { get; set; }
+
+        public List<OrderDetailsAddDto>? OrderDetails { get; set; }
+        public List<OrderCustomizationAddDto>? OrderCustomization { get; set; }
+    }
+}
