0f7f4a6e20d929813e78e5c15a30a047c4f842ae
iCtz <ahmed.t.alfusail@gmail.com>
2024-03-23 23:16:59 +0300

    FOOD-28 FOOD-5 #time 1m #in-progress FOOD-28 FOOD-5 #comment update globalUsing file

--- Utilities/GlobalUsings.cs
+++ Utilities/GlobalUsings.cs
@@ -3,6 +3,7 @@ global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
 global using OrderService.Contracts;
 global using OrderService.Controllers.Base;
+global using OrderService.Entities;
 global using OrderService.Entities.Model;
 global using OrderService.Entities.Model.DTOs;
 global using OrderService.Exceptions.Error;
