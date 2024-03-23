93b8ebef7c9d04d4263267595ca4b61680ca6d5a
iCtz <ahmed.t.alfusail@gmail.com>
2024-03-23 22:45:24 +0300

    FOOD-28 FOOD-5 #time 5m #in-progress FOOD-28 FOOD-5 #comment add order, orderdetails, & order customization config classes

---
+++ Entities/Configs/OrderConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Entities.Model;
+using System.Reflection.Emit;
+using System;
+
+namespace OrderService.Entities.Configs
+{
+    public class OrderConfig : IEntityTypeConfiguration<Order>
+    {
+        /// <summary>
+        /// Create a Index for [CustomerID, OrderDetailsID, OrderCustomization]
+        /// </summary>
+        /// <param name="builder"></param>
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasIndex(p => new { p.CustomerID, p.OrderDetails, p.OrderCustomization });
+        }
+    }
+}
---
+++ Entities/Configs/OrderDetailsConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace OrderService.Entities.Configs
+{
+    public class OrderDetailsConfig : IEntityTypeConfiguration<OrderDetails>
+    {
+        /// <summary>
+        /// Create a composit key for [OrderID, SeqID]
+        /// </summary>
+        /// <param name="builder"></param>
+        public void Configure(EntityTypeBuilder<OrderDetails> builder)
+        {
+            //builder.HasNoKey();
+
+            builder.HasKey(o => new { o.OrderID, o.Seq });
+        }
+    }
+}
